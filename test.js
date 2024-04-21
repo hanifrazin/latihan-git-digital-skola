74e043415b1b3eaa2f709a576b17507a62294a48
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2024-04-21 22:57:05 +0700

    add test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+let nama = "Hanif Razin Rahmatullah";
+let birthdate = "15 Juni 1995";
+let address = "Pamulang";
+let gender = "Pria";
+let religion = "Islam";
+let kewarganegaraan = "WNI";
+let email = "hanifrazin@outlook.com";
+let marital_status = "Belum Menikah"
+
+console.log("Biodata Diri");
+console.log(`Name : ${nama}`);
+console.log(`Tanggal Lahir : ${birthdate}`);
+console.log(`Alamat : ${address}`);
+console.log(`Jenis Kelamin : ${gender}`);
+console.log(`Agama : ${religion}`);
+console.log(`Kewarganegaraan : ${kewarganegaraan}`);
+console.log(`E-mail : ${email}`);
+console.log(`Status : ${marital_status}`);
